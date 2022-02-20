4e58d4bc757f16c28df429bc2b0153d8c64be94e
poulomi-biswas <poulomibiswas110@gmai.com>
2022-02-20 21:43:04 +0530

    apii

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const request = require("request");
 const https = require("https");
 const { options } = require("request");
 const path = require("path");
+require('dotenv').config();
 
 
 const app= express();
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.get("/", function(req,res){
 //     res.sendFile(__dirname + "/signup.html");
 // });
+
 
 app.post("/", function(req, res){
     const firstName= req.body.fName;
@@ -45,11 +47,11 @@ app.post("/", function(req, res){
    const request= https.request(url,options,function(response){
 
        if(response.statusCode ===200){
-           res.sendFile(__dirname + "/success.html");
+           res.sendFile(__dirname + "/public/success.html");
            
 
        }else{
-           res.sendFile(__dirname + "/failure.html");
+           res.sendFile(__dirname + "/public/failure.html");
        }
        response.on("data", function(data){
            console.log(JSON.parse(data));
